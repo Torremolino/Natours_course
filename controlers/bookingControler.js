0f284069f831c58390e6068917a903e57c59143a
Ricardo <fasgar.foros@gmail.com>
2020-12-30 20:43:13 +0100

    localizando errores

--- controlers/bookingControler.js
+++ controlers/bookingControler.js
@@ -57,10 +57,13 @@ exports.getCheckOutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
+  console.log(tour);
   const user = (await User.findOne({ email: session.customer_email })).id;
+  console.log(user);
   const price = session.display_items[0].amount / 100;
-  console.log('creado paquete');
+  console.log(price);
   await Booking.create({ tour, user, price });
+  console.log('creado paquete');
 };
 
 exports.webhookCheckout = (req, res, next) => {
